5979cb6156e8448cd35acb4c6c58eecff380fe59
Bstijn <baltessen.stijn@gmail.com>
2020-05-20 13:45:16 +0200

    wes-105: opzetten van cellservice

---
+++ CellService/Settings/DatastoreSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace CellService.Settings
+{
+   public interface ICellServiceDataStoreSettings
+    {
+        string UserCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+
+    public class CellServiceDatastoreSettings : ICellServiceDataStoreSettings
+    {
+        public string UserCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
+
